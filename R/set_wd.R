dbede90cc046ccb4b4d83e95b5b8d54c6730292f
ModelledCitizen <50756646+ModelledCitizen@users.noreply.github.com>
2020-02-21 13:32:19 -0500

    update documentation

--- R/set_wd.R
+++ R/set_wd.R
@@ -1,17 +1,20 @@
 #' Set Cloud-Aware Working Directory
 #'
-#' This function allows you to set a working directory within a given cloud storage provider in your home directory.
+#' This function allows you to set a working directory within a given cloud storage folder in your home directory.
+#'
 #' @param project The name of the project directory. Must be set.
 #' @param cloud The name of the cloud storage provider. If not set, the project is assumed to reside in your user folder.
 #' @keywords working directory, cloud storage, box, dropbox, odrive
 #' @export
 #' @examples
-#' set_wd()
+#' set_wd("Platforms", "Box")
+#' set_wd("Misogyny", "Dropbox")
+#' set_wd("rcp-scraping")
 
 set_wd <- function(project, cloud = NULL) {
   path <-
-    switch(Sys.info()[['sysname']], Windows = "C:/Users/", Darwin = "/Users/")
-  path <- paste0(path, Sys.info()[['user']], "/")
+    switch(Sys.info()[["sysname"]], Windows = "C:/Users/", Darwin = "/Users/")
+  path <- paste0(path, Sys.info()[["user"]], "/")
   if ("odrive" %in% list.files(path)) {
     cat("odrive detected; setting parent...\n")
     path <- paste0(path, "odrive/")
